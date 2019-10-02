706a22caa1ee691fd209f0bb2ed4ddf60274e3ef
David Rider <Rider.David@mayo.edu>
2019-10-02 16:45:13 -0500

    Completed most of the methods for chapter 16 with unit tests

--- mytime.py
+++ mytime.py
@@ -10,5 +10,63 @@ class Time:
         self.minute = 0
         self.second = 0
     
-def print_time(time):
-    print('%.2d:%.2d:%.2d' % (time.hour, time.minute, time.second))
+def compose_time(t):
+    return '%.2d:%.2d:%.2d' % (t.hour, t.minute, t.second)
+    
+def print_time(t):
+    print(compose_time(t))
+    
+def is_after(t1, t2):
+    return t1.hour > t2.hour or t1.minute > t2.minute or t1.second > t2.second
+    
+def add_time_simple(t1, t2):
+    sum = Time()
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+    return sum
+    
+def add_time_better(t1, t2):
+    sum = add_time_simple(t1, t2)
+    if sum.second >= 60:
+        sum.second -= 60
+        sum.minute += 1
+        
+    if sum.minute >= 60:
+       sum.minute -= 60
+       sum.hour += 1
+       
+    return sum
+    
+def increment_better(t, seconds):
+    seconds_time = Time()
+    seconds_time.second = seconds
+    return add_time_better(t, seconds_time)
+    
+def add_time(t1, t2):
+    sum = add_time_simple(t1, t2)
+    (add_minutes, seconds_remaining) = divmod(sum.second, 60)
+    sum.minute += add_minutes
+    sum.second = seconds_remaining
+    (add_hours, minutes_remaining) = divmod(sum.minute, 60)
+    sum.hour += add_hours
+    sum.minute = minutes_remaining
+    return sum
+    
+def increment(t, seconds):
+    seconds_time = Time()
+    seconds_time.second = seconds
+    return add_time(t, seconds_time)
+    
+def int_to_time(seconds):
+    t = Time()
+    minutes, t.second = divmod(seconds, 60)
+    t.hour, t.minute = divmod(minutes, 60)
+    return t
+    
+def time_to_int(t):
+    minutes = t.hour * 60 + t.minute
+    return minutes * 60 + t.second
+    
+def add_time_clever(t1, t2):
+    
--- test_mytime.py
+++ test_mytime.py
@@ -1,7 +1,5 @@
 import unittest
-from mytime import Time
-
-t = Time()
+from mytime import *
 
 class TestTime(unittest.TestCase):
     
@@ -18,8 +16,136 @@ class TestTime(unittest.TestCase):
         self.assertEqual(59, t.minute)
         self.assertEqual(30, t.second)
         
+    def test_compose_time(self):
+        t = Time()
+        self.assertEqual("00:00:00", compose_time(t))
+        t.hour = 11
+        t.minute = 59
+        t.second = 30
+        self.assertEqual("11:59:30", compose_time(t))
         
+    def test_is_after(self):
+        t1 = Time()
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertTrue(is_after(t2, t1))
+        self.assertFalse(is_after(t1, t2))
+        self.assertFalse(is_after(t1, t1))
+        self.assertFalse(is_after(t2, t2))
+        t3 = Time()
+        t3.hour = 11
+        t3.minute = 59
+        t3.second = 31
+        self.assertTrue(is_after(t3, t2))
+        self.assertFalse(is_after(t2, t3))
+        
+    def test_add_time_simple(self):
+        t1 = Time()
+        self.assertEqual("00:00:00", compose_time(add_time_simple(t1, t1)))
 
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertEqual("22:118:60", compose_time(add_time_simple(t2, t2)))
+        
+        # test example from the book
+        start = Time()
+        start.hour = 9
+        start.minute = 45
+        start.second = 0
+        duration = Time()
+        duration.hour = 1
+        duration.minute = 35
+        duration.second = 0
+        self.assertEqual("10:80:00", compose_time(add_time_simple(start, duration)))
+        
+    def test_add_time_better(self):
+        t1 = Time()
+        self.assertEqual("00:00:00", compose_time(add_time_better(t1, t1)))
+
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertEqual("23:59:00", compose_time(add_time_better(t2, t2)))
+   
+        # test example from the book
+        start = Time()
+        start.hour = 9
+        start.minute = 45
+        start.second = 0
+        duration = Time()
+        duration.hour = 1
+        duration.minute = 35
+        duration.second = 0
+        self.assertEqual("11:20:00", compose_time(add_time_better(start, duration)))
+
+    def test_increment_better(self):
+        t1 = Time()
+        self.assertEqual("00:00:00", compose_time(increment_better(t1, 0)))
+        self.assertEqual("00:00:30", compose_time(increment_better(t1, 30)))
+        self.assertEqual("00:01:20", compose_time(increment_better(t1, 80)))
+        self.assertEqual("00:01:195", compose_time(increment_better(t1, 255)))
+        
+    def test_add_time(self):
+        t1 = Time()
+        self.assertEqual("00:00:00", compose_time(add_time(t1, t1)))
+
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertEqual("23:59:00", compose_time(add_time(t2, t2)))
+        
+        # test example from the book
+        start = Time()
+        start.hour = 9
+        start.minute = 45
+        start.second = 0
+        duration = Time()
+        duration.hour = 1
+        duration.minute = 35
+        duration.second = 0
+        self.assertEqual("11:20:00", compose_time(add_time(start, duration)))   
+        
+        t3 = Time()
+        t3.hour = 23
+        t3.minute = 30
+        t3.second = 270
+        self.assertEqual("47:09:00", compose_time(add_time(t3, t3)))
+        
+    def test_increment(self):
+        t1 = Time()
+        self.assertEqual("00:00:00", compose_time(increment(t1, 0)))
+        self.assertEqual("00:00:30", compose_time(increment(t1, 30)))
+        self.assertEqual("00:01:20", compose_time(increment(t1, 80)))
+        self.assertEqual("00:04:15", compose_time(increment(t1, 255))) 
+       
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertEqual("12:04:20", compose_time(increment(t2, 290)))
+    
+    def test_int_to_time(self):
+        self.assertEqual("00:00:00", compose_time(int_to_time(0)))
+        self.assertEqual("00:00:30", compose_time(int_to_time(30)))
+        self.assertEqual("00:01:20", compose_time(int_to_time(80)))
+        self.assertEqual("00:04:15", compose_time(int_to_time(255))) 
+        
+    def test_time_to_int(self):
+        t1 = Time()
+        self.assertEqual(0, time_to_int(t1))
+        
+        t2 = Time()
+        t2.hour = 11
+        t2.minute = 59
+        t2.second = 30
+        self.assertEqual((11 * 3600) + (59 * 60) + 30, time_to_int(t2))
+        
 if __name__ == '__main__':
     unittest.main()
     
